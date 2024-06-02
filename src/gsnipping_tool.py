59efae233e4f0a8bef8adb4ddaf3355a5cdba5e6
Gavriel Linoy <kraserultra@gmail.com>
2024-06-03 00:19:01 +0300

    Add mouse listener with simple callback

--- src/gsnipping_tool.py
+++ src/gsnipping_tool.py
@@ -1,3 +1,20 @@
+import pyautogui
+from pynput import mouse
+
 class GSnippingTool(object):
     def __init__(self) -> None:
-        pass
+        self.__base_path: str = r'./'
+        
+        with mouse.Listener (
+            on_click=self.__on_click,
+        ) as listener:
+            listener.join()
+        
+    def __on_click(self,x, y, button, pressed):
+        print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
+        if not pressed:
+            # Stop listener
+            return False
+    
+    def take_screenshot(self, region: tuple = None) -> None:
+        pyautogui.screenshot(imageFilename=self.__base_path+'my_screenshot.png')
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from gsnipping_tool import GSnippingTool
 
 def main() -> None:
     gst = GSnippingTool()
+    gst.take_screenshot()
+    gst.take_screenshot()
 
 if __name__ == "__main__":
     main()
