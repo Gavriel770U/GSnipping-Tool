f28ee34b41e6d63a11feaf0a7ad2b8b18cc3d6f2
Gavriel Linoy <kraserultra@gmail.com>
2024-06-05 16:59:10 +0300

    Delete widgets and move to Toolbar with new cancel and options

--- src/gsnipping_tool_main_window.py
+++ src/gsnipping_tool_main_window.py
@@ -13,22 +13,25 @@ class GSnippingToolMainWindow(QMainWindow):
         self.main_layout = QHBoxLayout()
         self.main_widget.setLayout(self.main_layout)
         
-        self.new_screenshot_button = QPushButton(
-            text="New",
-            icon=QIcon("./icons/new.png")
-        )
-        self.cancel_screenshot_button = QPushButton(
-            text="Cancel",
-            icon=QIcon("./icons/cancel.png")
-        )
-        self.options_button = QPushButton(
-            text="Options",
-            icon=QIcon("./icons/options.png")
-        )
+        self.toolbar = QToolBar()
+        self.toolbar.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        self.addToolBar(self.toolbar)
         
-        self.main_layout.addWidget(self.new_screenshot_button)
-        self.main_layout.addWidget(self.cancel_screenshot_button)
-        self.main_layout.addWidget(self.options_button)
+        self.new_button_action = QAction(QIcon("./icons/new.png"), "New", self)
+        self.new_button_action.triggered.connect(lambda: print('Pressed New'))
+        self.new_button_action.setCheckable(True)
+        
+        self.cancel_button_action = QAction(QIcon("./icons/cancel.png"), "Cancel", self)
+        self.cancel_button_action.triggered.connect(lambda: print('Pressed Cancel'))
+        self.cancel_button_action.setCheckable(True)
+        
+        self.options_button_action = QAction(QIcon("./icons/options.png"), "Options", self)
+        self.options_button_action.triggered.connect(lambda: print('Options Pressed'))
+        self.options_button_action.setCheckable(True)
+        
+        self.toolbar.addAction(self.new_button_action)
+        self.toolbar.addAction(self.cancel_button_action)
+        self.toolbar.addAction(self.options_button_action)
         
         self.setCentralWidget(self.main_widget)
 
