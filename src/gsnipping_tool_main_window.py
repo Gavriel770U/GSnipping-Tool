87ce2794bf08c9a8a14c38122b31e31581e3e522
Gavriel Linoy <kraserultra@gmail.com>
2024-06-05 16:04:03 +0300

    Add main widget + main layout and add some buttons

--- src/gsnipping_tool_main_window.py
+++ src/gsnipping_tool_main_window.py
@@ -9,6 +9,19 @@ class GSnippingToolMainWindow(QMainWindow):
         
         self.setWindowTitle("GSnipping Tool")
         
+        self.main_widget = QWidget()
+        self.main_layout = QHBoxLayout()
+        self.main_widget.setLayout(self.main_layout)
+        
+        self.new_screenshot_button = QPushButton()
+        self.cancel_screenshot_button = QPushButton()
+        self.options_button = QPushButton()
+        
+        self.main_layout.addWidget(self.new_screenshot_button)
+        self.main_layout.addWidget(self.cancel_screenshot_button)
+        self.main_layout.addWidget(self.options_button)
+        
+        self.setCentralWidget(self.main_widget)
 
 app = QApplication(sys.argv)
 
