26248bebe64fdfcc0d4e04d83509c52ed516f185
Gavriel Linoy <kraserultra@gmail.com>
2024-06-05 16:06:35 +0300

    Add text to the buttons

--- src/gsnipping_tool_main_window.py
+++ src/gsnipping_tool_main_window.py
@@ -13,9 +13,15 @@ class GSnippingToolMainWindow(QMainWindow):
         self.main_layout = QHBoxLayout()
         self.main_widget.setLayout(self.main_layout)
         
-        self.new_screenshot_button = QPushButton()
-        self.cancel_screenshot_button = QPushButton()
-        self.options_button = QPushButton()
+        self.new_screenshot_button = QPushButton(
+            text="New"
+        )
+        self.cancel_screenshot_button = QPushButton(
+            text="Cancel"
+        )
+        self.options_button = QPushButton(
+            text="Options"
+        )
         
         self.main_layout.addWidget(self.new_screenshot_button)
         self.main_layout.addWidget(self.cancel_screenshot_button)
